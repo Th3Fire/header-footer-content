a2d8be4ee8ad553b7de92329535ed79a72ceda3b
wuttinunt <wuttinunt.th@gmail.com>
2016-03-13 03:38:49 +0700

    add index.php page

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Test php footer</title>
+</head>
+<body>
+
+<div id="header">
+	<center><h1>This Header...</h1></center>
+</div>
+<div id="content">
+	<h1>This Content...</h1>
+</div>
+<div id="footer">
+	<h1>This Footer...</h1>
+</div>
+
+</body>
+</html>
